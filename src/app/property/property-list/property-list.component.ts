bc77bd60463b4cd305bbaf2a8e0a75181a56db40
tuanpm <minhtuanq6@gmail.com>
2021-04-06 17:20:17 +0700

    Add CSS animation to Property Card

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  
+  properties: Array<any> = [
+    {
+      "Id" : 1,
+      "Type" : "House",
+      "Name" : "House",
+      "Price" : 12000
+    },
+    {
+      "Id" : 2,
+      "Type" : "House",
+      "Name" : "Villa",
+      "Price" : 12500
+    },
+    {
+      "Id" : 3,
+      "Type" : "House",
+      "Name" : "House 3",
+      "Price" : 14300
+    },
+    {
+      "Id" : 4,
+      "Type" : "House",
+      "Name" : "House 4",
+      "Price" : 18000
+    }
+  ]
 
   constructor() { }
 
