2d9e6e6bb3646817bda3d836a1021f1c541ef73c
minhtuan <minhtuanq6@gmail.com>
2021-04-14 23:43:56 +0700

    Reactive Form in Angular

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -13,12 +13,37 @@ export class UserRegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerationForm = new FormGroup({
-      userName: new FormControl('Mark', Validators.required),
+      userName: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-      confirmPassword: new FormControl(null, [Validators.required]),
-      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
-    });
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(15)])
+    }, this.passwordMatchingValidator);
+  }
+
+  passwordMatchingValidator(fg: FormGroup): Validators {
+    return fg.get('password').value === fg.get('confirmPassword').value ? null :
+    { notMatched: true };
+  }
+
+  get userName(){
+    return this.registerationForm.get('userName') as FormControl;
+  }
+  get email(){
+    return this.registerationForm.get('email') as FormControl;
+  }
+  get password(){
+    return this.registerationForm.get('password') as FormControl;
+  }
+  get confirmPassword(){
+    return this.registerationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile(){
+    return this.registerationForm.get('mobile') as FormControl;
+  }
+
+  userSubmitted(){
+    return false;
   }
 
   onSubmit(){
