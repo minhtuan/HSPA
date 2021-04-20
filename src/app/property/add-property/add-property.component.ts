9abf7d84b80c9506ac8d5823e37b0a14221b6403
tuanpm <minhtuanq6@gmail.com>
2021-04-20 11:46:18 +0700

    Two way binding

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -16,14 +17,17 @@ export class AddPropertyComponent implements OnInit {
   propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
 
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+
   constructor(private route: Router) { }
 
   ngOnInit() {
-    // this.addPropertyForm.control['Name'].setValue('Default Value');
-
-    setTimeout(() => {
-      // this.addPropertyForm.control['Name'].setValue('Default Value');
-    });
   }
 
   onBack()
