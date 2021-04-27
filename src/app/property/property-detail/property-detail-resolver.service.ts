611140d7964ca72d9dbc58ffbfdfc3295c8033eb
tuanpm <minhtuanq6@gmail.com>
2021-04-27 17:08:09 +0700

    using pipe

--- src/app/property/property-detail/property-detail-resolver.service.ts
+++ src/app/property/property-detail/property-detail-resolver.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Property } from 'src/app/model/property';
 import { HousingService } from 'src/app/services/housing.service';
 
@@ -9,11 +10,18 @@ import { HousingService } from 'src/app/services/housing.service';
 })
 export class PropertyDetailResolverService implements Resolve<Property>{
 
-constructor(private housingService: HousingService) { }
+constructor(private router: Router, private housingService: HousingService) { }
 
-resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Property> | Property {
-  const propId = +route.params['id'];
-  return this.housingService.getProperty(propId);
+resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): 
+  Observable<Property> | Property 
+{
+  const propId = route.params['id'];
+  return this.housingService.getProperty(+propId).pipe(
+    catchError(error => {
+      this.router.navigate(['/']);
+      return of(null);
+    })
+  );
 }
 
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -12,6 +12,7 @@ import { HousingService } from 'src/app/services/housing.service';
 export class PropertyListComponent implements OnInit {
   SellRent = 1;
   properties: IPropertyBase[];//Array<IPropertyBase>;
+  Today = new Date();
 
   constructor(private route: ActivatedRoute, private housingService: HousingService) { }
 
